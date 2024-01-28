1a0dccda038dc5205005e5e4d108ba811bd09fcb
nadavsc <nadavsch19ad@gmail.com>
2024-01-28 14:11:59 +0200

    updated shell scripts

--- MonoCoder/test.sh
+++ MonoCoder/test.sh
@@ -2,10 +2,10 @@
 
 python main.py      --do_test                                                           \
 		    --task mpi                                                          \
-                    --models_dir /home/nadavsc/shared/models/hf_checkpoints             \
-		    --model_name allc_gpt2tok_700M                                      \
+                    --models_dir ../models				                \
+		    --model_name monocoder_700M                                         \
 		    --max_length 2048                                                   \
                     --device cuda                                                       \
                     --tokenizer_type GPT2BPETokenizer                                   \
                     --logger mpi_bpe_orig.log                                           \
-		    --data_path /home/nadavsc/LIGHTBITS/mpiricalplus/dataset/dataset_saved/bpe/2048_test
+		    --data_path ../data/test
--- MonoCoder/train.sh
+++ MonoCoder/train.sh
@@ -2,13 +2,13 @@
 
 python main.py      --do_finetune                                                       \
 		    --task mpi                                                          \
-                    --models_dir /home/nadavsc/shared/models/hf_checkpoints             \
-		    --model_name allc_gpt2tok_700M                                      \
+                    --models_dir ../models					        \
+		    --model_name monocoder_700M                                         \
                     --batch_size 2                                                      \
 		    --max_length 2048                                                   \
                     --num_epochs 3                                                      \
                     --device cuda                                                       \
                     --tokenizer_type GPT2BPETokenizer                                   \
                     --logger mpi_bpe_orig.log                                           \
-		    --data_path /home/nadavsc/LIGHTBITS/mpiricalplus/dataset/dataset_saved/bpe/2048_train
+		    --data_path ../data/train
 
